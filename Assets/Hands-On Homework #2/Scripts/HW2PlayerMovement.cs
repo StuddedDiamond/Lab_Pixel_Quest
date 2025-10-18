271698226d00fe2601237f55cbb85ba78a177893
StuddedDiamond <shinhgodzilla@gmail.com>
2025-10-18 13:23:36 -0400

    Update

--- Assets/Hands-On Homework #2/Scripts/HW2PlayerMovement.cs
+++ Assets/Hands-On Homework #2/Scripts/HW2PlayerMovement.cs
@@ -2,5 +2,18 @@ using UnityEngine;
 
 public class HW2PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D _rigidbody2D;
 
+    private float _xSpeed;
+    private float _ySpeed;
+
+    private void Start()
+    {
+        _rigidbody2D=GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _xSpeed = Input.GetAxis("Horizontal")
+    }
 }
