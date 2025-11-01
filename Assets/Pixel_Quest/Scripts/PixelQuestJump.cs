c0920cd4f6a2b0a28b455307fa6c7ae85bdf731c
StuddedDiamond <shinhgodzilla@gmail.com>
2025-11-01 12:26:33 -0400

    jump

---
+++ Assets/Pixel_Quest/Scripts/PixelQuestJump.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelQuestJump : MonoBehaviour
+{
+
+    private Rigidbody2D _rigidbody2D;
+    public float jumpForce = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, jumpForce);
+            }
+        }
+    }
+}
--- Assets/Pixel_Quest/Scripts/PixelQuest_Movement.cs
+++ Assets/Pixel_Quest/Scripts/PixelQuest_Movement.cs
@@ -6,45 +6,24 @@ using UnityEngine.SceneManagement;
  public class PixelQuest_Movement : MonoBehaviour
     {
         Rigidbody2D rigidbody2D;
-        public float speed = 4;
+    SpriteRenderer spriteRenderer;
+        public int speed = 4;
 
         // Start is called before the first frame update
         void Start()
         {
             rigidbody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         }
 
         // Update is called once per frame
         void Update()
         {
-            float xInput = Input.GetAxis("Horizontal");
-            rigidbody2D.velocity = new Vector2(xInput * speed, rigidbody2D.velocity.y);
-
-            if (Input.GetKeyDown(KeyCode.W))
-            {
-                rigidbody2D.velocity += new Vector2(rigidbody2D.velocity.x, -1);
-            }
-
-            if (Input.GetKeyDown(KeyCode.S))
-            {
-                rigidbody2D.velocity += new Vector2(rigidbody2D.velocity.x, 3);
-            }
+        float horizontal = Input.GetAxis("Horizontal"); 
+        if (horizontal > 0) { spriteRenderer.flipX = true; }
+        else if (horizontal < 0) { spriteRenderer.flipX= false; }
+            rigidbody2D.velocity = new Vector2(horizontal * speed, rigidbody2D.velocity.y);        
         }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Debug.Log("Hit");
-        switch (collision.tag)
-        {
-            case "Death":
-                {
-                    string thisLevel = SceneManager.GetActiveScene().name;
-                    SceneManager.LoadScene(thisLevel);
-                    break;
-                }
-        }
-
-    }
 }
 
     
